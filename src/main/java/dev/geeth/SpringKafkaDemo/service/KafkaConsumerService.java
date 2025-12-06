e7dbe3975fb34579ff13f021a5796f6df9b94260
Sulakshana Geeth <sulakshanageeth@gmail.com>
2025-12-06 23:46:37 +0530

    create kafca consumer and producer

---
+++ src/main/java/dev/geeth/SpringKafkaDemo/service/KafkaConsumerService.java
@@ -0,0 +1,13 @@
+package dev.geeth.SpringKafkaDemo.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "demo-topic", groupId = "demo-group")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
